d2f193e785c1b5197e489e286a0e727894816713
wesleygomes30-web <wesley.gomes30@escola.pr.gov.br>
2026-08-24 11:36:55 -0300

    Create  joins-assinantes.sql

---
+++ joins-assinantes.sql
@@ -0,0 +1,6 @@
+https://www.db-fiddle.com/f/jhJySCYdjxZxsqaAeNMB49/0
+https://www.db-fiddle.com/f/jhJySCYdjxZxsqaAeNMB49/1
+https://www.db-fiddle.com/f/jhJySCYdjxZxsqaAeNMB49/2
+https://www.db-fiddle.com/f/jhJySCYdjxZxsqaAeNMB49/3
+https://www.db-fiddle.com/f/jhJySCYdjxZxsqaAeNMB49/4
+https://www.db-fiddle.com/f/jhJySCYdjxZxsqaAeNMB49/5
